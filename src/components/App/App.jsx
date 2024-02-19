21268a59cf9acc4ed7ef3ac78bd0ae5b43571b11
Ld-monkey <lfourteau91@gmail.com>
2024-02-19 16:51:47 +0100

    Add pagination with button

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,14 +2,22 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import NavBar from '../NavBar/NavBar';
 import Pokedex from '../Pokedex/Pokedex';
+import './App.scss';
 
 function App() {
   const [pokedex, setPokedex] = useState();
+  const [countPagination, setCountPagination] = useState(1);
+
+  function handleLoadMore() {
+    // const count = countPagination + 1;
+    setCountPagination(countPagination + 1);
+  }
 
   async function fetchPokedexData() {
     try {
+      // 0 à 9.
       const response = await axios.get(
-        'https://pokebuildapi.fr/api/v1/pokemon/limit/10',
+        `https://pokebuildapi.fr/api/v1/pokemon/limit/${countPagination * 10}`,
       );
       if (response.data && response.data.length) {
         setPokedex(response.data);
@@ -21,12 +29,15 @@ function App() {
 
   useEffect(() => {
     fetchPokedexData();
-  }, []);
+  }, [countPagination]);
 
   return (
     <main>
       <NavBar />
       {pokedex && <Pokedex data={pokedex} />}
+      <button type="button" onClick={handleLoadMore} className="button-loading">
+        Load more
+      </button>
     </main>
   );
 }
